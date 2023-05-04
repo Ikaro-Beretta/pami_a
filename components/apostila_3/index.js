606251fd30d8a24dcb79e8fe39df5a3bb4f36a35
Ikaro-Beretta <124743573+Ikaro-Beretta@users.noreply.github.com>
2023-05-04 20:24:30 -0300

    aula_04

--- components/apostila_3/index.js
+++ components/apostila_3/index.js
@@ -1,22 +1,40 @@
-import *as react from 'react';
-import {view,text,stylesheet} from 'react-native';
+import * as React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { View, Text, Stylesheet } from 'react-native';
 
-export default function index(){
+export default function Index() {
     return(
         <view style={styles.container}>
-          <text style={styles.paragraph}>
-          exemplo 3
-          </text>
-        </view>     
-     );
+            <Text style={styles.paragraph}>
+                Exmplo 3
+            </Text>
+
+            <TouchableOpacity
+                onPress={() => alert ('Ola mundo!!!') }
+                style={styles.button}
+            > 
+
+                <text style={styles.textbutton}>
+                  Ola mundo!!!
+                </text>   
+            </TouchableOpacity>
+
+        </view>
+    );
 }
 
-const styles = stylesheet.create({
+const styles = Stylesheet.create({
     container: {
         flex: 1,
-        justifycontent'center',
-        backgroundcolor: '#f5f5f5',
-        padding:8,
+        justifyContent: 'center',
+        backgroundColor: '#f5f5f5',
+        padding: 8,
     },
-    
-})
+    paragraph: {
+        margin: 24,
+        fontSize: 26,
+        fontweight: 'bold',
+        textalign: 'center',
+        color: '#9e9e9e',
+    },
+});
