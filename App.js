0448aa9761494fc9efc0cadcc2446e8a81f2a4d3
Ikaro-Beretta <124743573+Ikaro-Beretta@users.noreply.github.com>
2023-06-06 20:52:09 -0300

    apostila05

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native'; 
 
-import Apostila03 from './components/apostila_03';
-import Apostila03A from './components/apostila_03 _avancado';
-import Apostila04 from './components/apostila_04';
+//import Apostila03 from './components/apostila_03';
+//import Apostila03A from './components/apostila_03 _avancado';
+//import Apostila04 from './components/apostila_04';
+import Apostila05 from './components/Apostila_05';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila04 />
+      <Apostila05 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Apostila_05/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+export default function Index(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+            Exemplo 5
+        </Text>
+      </View>
+    );
+}
+
+
+
