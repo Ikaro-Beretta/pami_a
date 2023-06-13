046846e6388e3b3342e27a2464154b9fa15316e1
Ikaro-Beretta <124743573+Ikaro-Beretta@users.noreply.github.com>
2023-06-13 20:20:37 -0300

    13-05

---
+++ components/Apostila_06/index.js
@@ -0,0 +1,12 @@
+import React,{ useState } from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native/types';
+import styles from '../Apostila_05/styles';
+
+export default function Index() {
+    return(
+    <View style={styles.container}>
+      <Text style={styles.paragraph}> Exemplo 6 </Text>  
+    </View>
+    );
+}
+
